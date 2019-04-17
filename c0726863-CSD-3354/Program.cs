9e1c41c1697d0eb7c590dee922e0000618053082
visualstudio <visualstudio@122-PC13>
2019-04-17 10:23:32 -0400

    LongestWord

--- c0726863-CSD-3354/Program.cs
+++ c0726863-CSD-3354/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0726863_CSD_3354
+namespace LongestWord
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("The Longest Word is : ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+        }
+
+        public void MyFunctionA(string input)
+        {
+            string[] words = input.Split(' ');
+
+            int wordArrayLength = words.Length;
+            int[] wordsLenth = new int[wordArrayLength];
+            int x = 0;
+
+            foreach (var word in words)
+            {
+
+            }
+            string currentWord = words[0];
+            string nextWord;
+            string longestWord = currentWord;
+
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+
+            }
         }
     }
 }
+
